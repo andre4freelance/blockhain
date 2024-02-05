ba1fe2f3c23ccd49d6404ecb0e6ce9102c6c7f0e
Andre Bastian <152775162+andre4freelance@users.noreply.github.com>
2024-02-06 03:06:53 +0700

    Add files via upload
    add sync multiple node (Decentralized)

--- blockchain.py
+++ blockchain.py
@@ -21,6 +21,8 @@ class Blockchain(object):
             return hashlib.sha256(block_encoded).hexdigest() 
 
     def __init__(self):
+        self.nodes = set()
+
         self.chain = []
 
         self.current_transaction = []   
@@ -31,6 +33,57 @@ class Blockchain(object):
             hash_opb = genesis_hash,
             nonce = self.pow(0, genesis_hash, [])
         )
+
+    def add_node(self, addresss):
+         parse_url = urlparse(addresss)
+         self.nodes.add(parse_url.netloc)
+         print(parse_url.netloc )
+      
+    def valid_chain(self, chain):
+         last_block = chain[0]
+         current_index = 1
+
+         while current_index < len(chain):
+              block = chain[current_index]
+
+              if block['hash_opb'] != self.hash_block(last_block):
+                   return False
+              
+              if not self.valid_prof(
+                   current_index,
+                   block['hash_opb'],
+                   block['transaction'],
+                   block['nonce']):
+                   return False
+              
+
+              last_block = block
+              current_index += 1
+
+         return True
+    
+    def update_blockchain(self):
+       neigbours = self.nodes
+       new_chain = None
+
+       max_length = len(self.chain)
+
+       for node in neigbours:
+            response = requests.get(f'http://{node}/blockchain')
+
+            if response.status_code == 200:
+                 length = response.json()['length']
+                 chain = response.json()['chain']
+
+                 if length > max_length and self.valid_chain(chain):
+                      max_length = length
+                      new_chain = chain
+
+                 if new_chain:
+                      self.chain = new_chain
+                      return True
+                 
+            return False
 
     def pow(self, index, hash_opb, transaction):
         nonce = 0
@@ -130,6 +183,41 @@ def new_transaction():
 
      response = {'message': f'Transaksi akan  di tambahkan ke block {index}'}
      return (jsonify(response), 201)
+
+
+@app.route('/nodes/add_nodes', methods=['POST'])
+def add_nodes():
+     values = request.get_json()
+     nodes = values.get('nodes')
+
+     if nodes is None:
+          return "Error, missing node(s) info", 400
+     
+     for node in nodes:
+          blockchain.add_node(node)
+
+     response = {
+          'message': 'Node baru telah di tambahkan',
+          'nodes': list(blockchain.nodes)
+     }
+
+     return jsonify(response), 200
+
+@app.route('/nodes/sync', methods=['GET'])
+def sync():
+     updated = blockchain.update_blockchain()
+     if updated:
+          response = {
+               'message': 'Blockchain berhasil di update dari node terpanjang',
+               'blockchain': blockchain.chain
+          }
+     else:
+          response = {
+               'message': 'Blockchain uptodate',
+               'blockchain': blockchain.chain
+          }
+
+     return jsonify(response), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(sys.argv[1]))
